cb6e84536096108b3b0c07158139551bbc47cbfd
Trần Nguyện <kidnguyenkun@gmail.com>
2020-09-22 06:29:58 +0700

    init component

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { CategoryComponent } from './category/category.component';
+import { ContactComponent } from './contact/contact.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    CategoryComponent,
+    ContactComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
