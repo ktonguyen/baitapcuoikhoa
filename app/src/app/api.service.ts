d76911bec2e948599e491f297d77253601f305aa
Trần Nguyện <kidnguyenkun@gmail.com>
2020-09-22 07:00:48 +0700

    init service

--- app/src/app/api.service.ts
+++ app/src/app/api.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+ 
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  constructor(private http:HttpClient) {}
 
-  constructor() { }
+  get(api, data) {
+    return this.http.get(api, httpOptions);
+  }
+  put(api, data) {
+    let body = JSON.stringify(data)
+    return this.http.put(api, body, httpOptions);
+  }
+  post(api, data) {
+    let body = JSON.stringify(data)
+    return this.http.post(api, body, httpOptions);
+  }
+  delete(api, data) {
+    return this.http.delete(api, httpOptions);
+  }
 }
